bb607d289aba1ae539b993c799f7d8106056738b
Marcvs101 <marcocuoci@gmail.com>
2018-09-27 13:09:13 +0200

    Add files via upload

--- Fiber.c
+++ Fiber.c
@@ -1,29 +1,49 @@
 #include <linux/module.h>	// Needed by all modules
 #include <linux/kernel.h>	// Needed for KERN_INFO
+#include <linux/init.h>		// Macro __init,__exit
+//#include <sys/ioctl.h>
+#include <linux/fs.h>
 #include "Strutture.h"
 #include "Fls.h"
 
-int init_module(void){
+const struct file_operations fops ={
+	.open		= NULL, //try_module_get(THIS)
+	.release	= NULL, //module_put(THIS)
+	.read		= NULL,
+	.write		= NULL,
+};
+
+static int __init ingresso(void){
+	int num = register_chrdev(666,"LinuxFibers",fops);
 	printk(KERN_INFO "CARICATO FIBER.KO\n");
 	return 0;
 }
 
-void cleanup_module(void){
+static void __exit uscita(void){
 	printk(KERN_INFO "RIMOSSO FIBER.KO\n");
 }
 
-int ConvertThreadToFiber(){
+module_init(ingresso);
+module_exit(uscita);
+
+// Funzioni
+
+static int ConvertThreadToFiber(){
 	//Controlla se sto già runnando una fiber
+	//Se sei il primo tira su le strutture di controllo
+	//Chiama CreateFiber
+	//Switcha sul nuovo fiber
 	return 0;
 }
 
-long CreateFiber(void* code){ //Parametri?
+static long CreateFiber(void* code){ //Parametri?
 	//Crea le strutture
 	//Popola le strutture
+	//Crea entry su proc
 	//Ritorna il fiber id
 }
 
-void SwitchToFiber(long id){
+static void SwitchToFiber(long id){
 	//Controlla se la fiber "id" esiste
 	//Recupera le strutture
 	//Salva lo stato, ripristina il nuovo stato
