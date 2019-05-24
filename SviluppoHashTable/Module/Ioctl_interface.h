e94fa7e52aa8db9322a3d9764bd23dfa9eff411f
Dario Litardi <dario.litardi@libero.it>
2019-05-24 12:14:19 +0200

    Add files via upload

--- SviluppoHashTable/Module/Ioctl_interface.h
+++ SviluppoHashTable/Module/Ioctl_interface.h
@@ -1,9 +1,9 @@
 #define IOCTL_MAGIC 'f'
 
-#define FIB_FLS_ALLOC _IOR(IOCTL_MAGIC, 10, struct fiber_arguments)
-#define FIB_FLS_GET	_IOWR(IOCTL_MAGIC, 2, struct fiber_arguments)
-#define FIB_FLS_SET _IOW(IOCTL_MAGIC, 3, struct fiber_arguments)
-#define FIB_FLS_DEALLOC	_IOW(IOCTL_MAGIC, 4, struct fiber_arguments)
+#define FIB_FLS_ALLOC _IOR(IOCTL_MAGIC, 10, struct fiber_arguments* )
+#define FIB_FLS_GET	_IOWR(IOCTL_MAGIC, 2, struct fiber_arguments* )
+#define FIB_FLS_SET _IOW(IOCTL_MAGIC, 3, struct fiber_arguments* )
+#define FIB_FLS_DEALLOC	_IOW(IOCTL_MAGIC, 4, struct fiber_arguments* )
 #define FIB_CONVERT	_IO(IOCTL_MAGIC, 5)
-#define FIB_CREATE _IOWR(IOCTL_MAGIC, 6, struct fiber_arguments)
-#define FIB_SWITCH_TO _IOW(IOCTL_MAGIC, 7, struct fiber_arguments)
+#define FIB_CREATE _IOWR(IOCTL_MAGIC, 6, struct fiber_arguments* )
+#define FIB_SWITCH_TO _IOW(IOCTL_MAGIC, 7, struct fiber_arguments* )
